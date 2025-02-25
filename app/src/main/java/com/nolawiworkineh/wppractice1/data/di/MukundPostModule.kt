fc183a7644c1d86746907d66595a46fe1eea8ce1
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-02-25 13:08:48 -0800

    providing the retrofit client to the module

--- app/src/main/java/com/nolawiworkineh/wppractice1/data/di/MukundPostModule.kt
+++ app/src/main/java/com/nolawiworkineh/wppractice1/data/di/MukundPostModule.kt
@@ -1,5 +1,7 @@
 package com.nolawiworkineh.wppractice1.data.di
 
+import com.nolawiworkineh.wppractice1.data.PostsModel
+import com.nolawiworkineh.wppractice1.data.RetrofitClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -7,3 +9,12 @@ import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
+object MukundPostModule{
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = RetrofitClient.retrofit
+
+}
