5f9bd58565547526983b7a0119e9196e8a87dc5c
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-02-25 13:04:57 -0800

    adding the retrofit client

---
+++ app/src/main/java/com/nolawiworkineh/wppractice1/data/RetrofitClient.kt
@@ -0,0 +1,15 @@
+package com.nolawiworkineh.wppractice1.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
