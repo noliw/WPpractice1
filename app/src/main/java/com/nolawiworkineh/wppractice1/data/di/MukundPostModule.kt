c62714cb87b9efa82469bf40a3f79bf3cb8e9e74
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-02-25 04:07:59 -0800

    setting up the post module

--- app/src/main/java/com/nolawiworkineh/wppractice1/data/di/MukundPostModule.kt
+++ app/src/main/java/com/nolawiworkineh/wppractice1/data/di/MukundPostModule.kt
@@ -1,10 +1,25 @@
 package com.nolawiworkineh.wppractice1.data.di
 
+import com.nolawiworkineh.wppractice1.data.MukundApiService
+import com.nolawiworkineh.wppractice1.data.MukundsRetrofitClient
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object MukundPostModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = MukundsRetrofitClient.retrofit
+
+    @Provides
+    @Singleton
+    fun providePostApiService(retrofit: Retrofit): MukundApiService =
+        retrofit.create(MukundApiService::class.java)
+
 }
